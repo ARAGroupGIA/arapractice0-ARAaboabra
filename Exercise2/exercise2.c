0d8c4d833bf598ce825c512fa82494e23d9bac04
ARAaboabra <55189026+ARAaboabra@users.noreply.github.com>
2019-09-11 14:32:47 +0200

    exercise 2 practise 1

--- Exercise2/exercise2.c
+++ Exercise2/exercise2.c
@@ -1,5 +1,18 @@
 /* Here, you should entry the required libraries */
+#include <stdio.h>
+
 
 void main(){
+	int num1;
+	int num2;
+	int result=0;
+
 	/* Here, you should write the source code of your program */
+	printf("Introduce the first number you want to sum \n" );
+	scanf("%d",&num1);
+	printf("Introduce the second number you want to sum \n " );
+	scanf("%d",&num2);
+	result = num1+num2;
+	
+	printf("the result of the sum is %d", &result);
 }
